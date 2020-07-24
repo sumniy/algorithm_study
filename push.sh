2de9c8b2d28237f8a31727a777c423e16df8644c
sumniy <sumniy94@gmail.com>
2020-07-24 23:08:27 +0900

    add push.sh if statement

--- push.sh
+++ push.sh
@@ -8,10 +8,14 @@ git add .
 
 echo "> add all files"
 
+if [ $# = 1 ]; then
+  read COMMIT_MESSAGE
+else 
+  COMMIT_MESSAGE={$1}
+fi
+
 COMMIT_MESSAGE=${1}
 git commit -m "${COMMIT_MESSAGE}"
-
-
 
 echo "> commit complte"
 
