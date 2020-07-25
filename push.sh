72583781f83a4295c6a259c43787ffcef4535c21
sumniy <sumniy94@gmail.com>
2020-07-25 21:27:19 +0900

    complete final push.sh

--- push.sh
+++ push.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 
-# pull, add, commit, push 각 과정이 실패했을 때를 핸들링해주고
-# 현재 작업하는 remote가 어떤 지 알아내고, 브랜치를 알아내고 그 브랜치에 push 하도록
-# 충돌 발생 시 중간에 멈춰주는 것도 하고,
-# 그담에 만약에 add하고 commit할 때 에러나면 add 했던 거 취소도 해주고 그렇게 구현하자
-
 REMOTE_REPO=$(git remote)
 WORKING_BRANCH=$(git branch --show-current)
 
@@ -16,9 +11,7 @@ git add .
 
 echo "> add all files"
 
-# push.sh 실행 시 매개변수를 전달하지 않으면 입력받고, 전달받으면 바로 COMMIT_MESSAGE에 저장한다.
 if [ $# -eq 0 ]; then
-  # read에 -p 옵션을 주면 입력한 프롬프트를 띄우고 입력을 대기한다.
   read -p "> enter commit message : " COMMIT_MESSAGE
 else 
   COMMIT_MESSAGE=${1}
